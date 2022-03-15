effa6fc75154faf8688b8a5521d9ee817176d578
Kyle Jordan <kjord021@fiu.edu>
2022-03-15 12:20:24 -0400

    unused imports]

--- src/CSVWriter.java
+++ src/CSVWriter.java
@@ -1,12 +1,10 @@
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.List;
 
 public class CSVWriter {
 
